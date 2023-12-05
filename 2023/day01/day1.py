c6b8668408ae9e1b082d784c35a50bed3a0c2781
Tarek Hiemstra <tarek.hiemstra@gmail.com>
2023-12-05 17:49:00 +0200

    Add Advent of Code 2023 day 1 part 1&2

---
+++ 2023/day01/day1.py
@@ -0,0 +1,44 @@
+# Day 1 part 1
+sum = 0
+with open('day1.txt') as file:
+    lines = file.readlines()
+    for line in lines:
+        my_list = list()
+        for c in line:
+            if c.isnumeric():
+                my_list.append(c)
+        line_value = str(my_list[0]) + str(my_list[-1])
+        sum += int(line_value)
+print(sum)
+
+
+# Day 1 part 2
+sum = 0
+with open('day1.txt') as file:
+    lines = file.readlines()
+    for line in lines:
+        numbers = {
+            "one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+            "six": 6, "seven": 7, "eight": 8, "nine": 9, 
+            "1": 1, "2": 2, "3": 3, "4": 4, "5": 5,
+            "6": 6, "7": 7, "8": 8, "9": 9
+        }
+        
+        lowest_index = len(line)
+        highest_index = 0
+        first_number = len(line)
+        last_number = 0
+        
+        for number in numbers:
+            if number in line:
+                first_occurence = line.find(number)
+                last_occurence = line.rfind(number)
+                if first_occurence <= lowest_index:
+                    lowest_index = first_occurence
+                    first_number = numbers.get(number)
+                if last_occurence >= highest_index:
+                    highest_index = last_occurence
+                    last_number = numbers.get(number)
+        line_value = str(first_number) + str(last_number)
+        sum += int(line_value)
+print(sum)
